51c5bad275c821a493c123fea08a6784e9b9cfa2
Vipin Kumar <kumarvipin88073@gmail.com>
2025-09-23 14:24:40 +0530

    added instance_crn for upgrade test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -203,6 +203,7 @@ func TestFullyConfigurableUpgradeSolution(t *testing.T) {
 			{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 			{Name: "cluster_id", Value: terraform.Output(t, existingTerraformOptions, "cluster_id"), DataType: "string"},
 			{Name: "cluster_resource_group_id", Value: terraform.Output(t, existingTerraformOptions, "cluster_resource_group_id"), DataType: "string"},
+			{Name: "instance_crn", Value: terraform.Output(t, existingTerraformOptions, "instance_crn"), DataType: "string", Secure: true},
 			{Name: "access_key", Value: terraform.Output(t, existingTerraformOptions, "access_key"), DataType: "string", Secure: true},
 		}
 
