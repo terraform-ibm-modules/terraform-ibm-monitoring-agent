d7659a69b6e16382cc21403cfd20cbbdaccbd25c
Vipin Kumar <kumarvipin88073@gmail.com>
2025-09-24 15:49:25 +0530

    added provider visibility

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -387,3 +387,14 @@ variable "cluster_shield_limits_memory" {
   description = "Specify memory resource limits for the cluster shield pods."
   default     = "1536Mi"
 }
+
+variable "provider_visibility" {
+  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
+  type        = string
+  default     = "private"
+
+  validation {
+    condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
+    error_message = "Invalid visibility option. Allowed values are 'public', 'private', or 'public-and-private'."
+  }
+}
