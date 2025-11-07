fdc71b03a704c55e9e2be814633c749cc526021c
raagamounica <mounica.tellabati@ibm.com>
2025-11-07 20:37:28 +0530

    feat: Added next steps url for IBM cloud monitoring agent

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -1,6 +1,33 @@
 ##############################################################################
 # Outputs
 ##############################################################################
+##############################################################################
+#  Monitoring and Workload Protection agent- Next Steps URLs outputs
+##############################################################################
 
+output "next_steps_text" {
+  value       = "Your Monitoring Instance Environment is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Monitoring Instance Dashboard" 
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/observability/embedded-view/monitoring/${element(split(":", var.instance_crn), 7)}"
+  description = "Primary URL"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about IBM Cloud Monitoring"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://cloud.ibm.com/docs/monitoring?topic=monitoring-getting-started"
+  description = "Secondary URL"
+}
 
 ##############################################################################
