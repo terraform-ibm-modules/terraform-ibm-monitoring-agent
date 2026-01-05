b50a2d062bf9c15b8cd5306a8683a5b3ca95e364
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-01-05 14:55:30 +0530

    fix(deps): update dependencies

--- examples/obs-agent-ocp/version.tf
+++ examples/obs-agent-ocp/version.tf
@@ -14,7 +14,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.16.1"
+      version = ">= 3.0.1"
     }
     restapi = {
       source  = "Mastercard/restapi"
