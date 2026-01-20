9d25d3258514e524e0d148efef570cadb746729f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-20 12:28:11 +0000

    fix(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -71,7 +71,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.77.3"
+  version              = "3.77.7"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
@@ -90,7 +90,7 @@ module "ocp_base" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.12.16"
+  version           = "1.12.19"
   instance_name     = "${var.prefix}-cloud-monitoring"
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
