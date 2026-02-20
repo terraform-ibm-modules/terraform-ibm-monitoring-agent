c0880a5667c0329f4463a108188f18ad91824113
Harika-Ponna <Harika.Ponna@ibm.com>
2026-02-20 09:40:02 +0530

    fix: update var

--- variables.tf
+++ variables.tf
@@ -450,6 +450,6 @@ variable "cluster_shield_limits_memory" {
 variable "install_required_binaries" {
   type        = bool
   default     = true
-  description = "When set to true, a script will run to check if `kubectl` exist on the runtime and if not attempt to download it from the public internet and install it to /tmp. Set to false to skip running this script."
+  description = "When set to true, a script will run to check if `kubectl` exists on the runtime and if not attempt to download it from the public internet and install it to /tmp. Set to false to skip running this script."
   nullable    = false
 }
