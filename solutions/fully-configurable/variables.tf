b9ab0726e62c7498f6e48cbaa6275477940c852d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-13 00:20:39 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -186,7 +186,7 @@ variable "chart_version" {
   description = "The version of the agent helm chart to deploy."
   type        = string
   # This version is automatically managed by renovate automation - do not remove the registryUrl comment on next line
-  default  = "1.105.1" # registryUrl: charts.sysdig.com
+  default  = "1.106.1" # registryUrl: charts.sysdig.com
   nullable = false
 }
 
@@ -215,7 +215,7 @@ variable "agent_image_tag_digest" {
   description = "The image tag or digest of agent image to use. If using digest, it must be in the format of `X.Y.Z@sha256:xxxxx`. This version must match the version being used in the `kernel_module_image_digest`."
   type        = string
   # This version is automatically managed by renovate automation - do not remove the datasource comment on next line
-  default  = "14.4.0@sha256:7546019a9fc55650a5d73825b2ebfcd6f49f435102c35b87249ae163cb9cf22f" # datasource: icr.io/ext/sysdig/agent-slim
+  default  = "14.4.1@sha256:579a0fbc9c32e4263b54444b60689f2c16caa1c4f5518430f9b71566a0870bd5" # datasource: icr.io/ext/sysdig/agent-slim
   nullable = false
 }
 
@@ -223,7 +223,7 @@ variable "kernel_module_image_digest" {
   description = "The image digest to use for the agent kernel module used by the initContainer. Must be in the format of `X.Y.Z@sha256:xxxxx`. This version must match the version being used in the `agent_image_tag_digest`. Note: Only digest format is supported; image tag is not supported."
   type        = string
   # This version is automatically managed by renovate automation - do not remove the datasource comment on next line
-  default  = "14.4.0@sha256:e25b121856597f0443c0eb9f3201519abc98021f71c807d2a04bc0c9fc7529a2" # datasource: icr.io/ext/sysdig/agent-kmodule
+  default  = "14.4.1@sha256:bbee1ada9944f7cc3b4e868e1ccfc4d5088e2dffef686e558ec3422e65243d49" # datasource: icr.io/ext/sysdig/agent-kmodule
   nullable = false
   validation {
     condition     = can(regex("^\\d+\\.\\d+\\.\\d+@sha256:[a-f0-9]{64}$", var.kernel_module_image_digest))
