9a3874f6efc1a3e4824752cc499f5645f95122e4
Md Anam Raihan <md.anam.raihan17@gmail.com>
2026-07-10 15:23:52 +0530

    fix(deps): update dependencies (#353)
    * fix(deps): update dependencies
    
    * update tags to reosurce_tags
    
    * resolve test resources issue
    
    ---------
    
    Co-authored-by: Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
    Co-authored-by: Md Anam Raihan <md.anam.raihan@ibm.com>

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -71,10 +71,10 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.89.0"
+  version              = "3.90.2"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
-  tags                 = var.resource_tags
+  resource_tags        = var.resource_tags
   cluster_name         = "${var.prefix}-cluster"
   force_delete_storage = true
   vpc_id               = ibm_is_vpc.vpc.id
@@ -90,7 +90,7 @@ module "ocp_base" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.15.8"
+  version           = "1.15.11"
   instance_name     = "${var.prefix}-cloud-monitoring"
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
