43a8a3127c3500627ac9fa01f9b46d319301e7d4
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-06 16:44:12 +0530

    chore(deps): update terraform terraform-ibm-modules/scc-workload-protection/ibm to v1.22.3 (#369)

--- examples/obs-agent-ocp/main.tf
+++ examples/obs-agent-ocp/main.tf
@@ -124,7 +124,7 @@ module "app_config" {
 
 module "scc_wp" {
   source                                       = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version                                      = "1.22.2"
+  version                                      = "1.22.3"
   name                                         = "${var.prefix}-scc-wp"
   resource_group_id                            = module.resource_group.resource_group_id
   region                                       = var.region
