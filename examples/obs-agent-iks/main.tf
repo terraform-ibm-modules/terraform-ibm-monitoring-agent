1edfea162756c0230c90632e493c1ff0f44e5532
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-20 23:28:36 +0000

    chore(deps): update dependencies

--- examples/obs-agent-iks/main.tf
+++ examples/obs-agent-iks/main.tf
@@ -137,7 +137,7 @@ module "app_config" {
 
 module "scc_wp" {
   source                                       = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version                                      = "1.22.10"
+  version                                      = "1.22.12"
   name                                         = "${var.prefix}-scc-wp"
   resource_group_id                            = module.resource_group.resource_group_id
   region                                       = var.region
